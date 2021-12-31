7fbee93108367e82cc7f71f59567d606b6dfb7d1
Chris Dzombak <chris@dzombak.com>
2021-12-31 10:18:46 -0500

    switch to rising-edge 1PPS interrupt

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ extern "C" int _gettimeofday_r(struct _reent* unused, struct timeval *tp, void *
 #define LOCK_PIN D5
 #define TICK_PIN D6
 #define SVC_PIN  D7
-#define TICK_EDGE FALLING  // rising & change don't work, likely due to poor performance of voltage divider as level shifter
+#define TICK_EDGE RISING
 // #define OSC_IGNORE_LOCK
 // #define OSC_FORCE_SERVICE
 
